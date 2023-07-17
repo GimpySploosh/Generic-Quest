2d829483b1da8f53c4bc27cea4c03eacae96d389
GimpySploosh <86210959+GimpySploosh@users.noreply.github.com>
2023-07-17 10:07:42 -0400

    Flexible inputs for sprite class

--- introStuff.py
+++ introStuff.py
@@ -1,27 +1,23 @@
 import pygame
 
+SCALE = 1
 playerImage = pygame.image.load("player.png")
 
-# Resize the image to a smaller size
-playerWidth = 25
-playerHeight = 25
-playerImage = pygame.transform.scale(playerImage, (playerWidth, playerHeight))
-
 class MySprite(pygame.sprite.Sprite):
-    def __init__(self, image, x, y):
+    def __init__(self, image, x, y, width, height):
         super().__init__()
-        self.image = image
+        self.image = pygame.transform.scale(image, (width * SCALE, height * SCALE))
         self.rect = self.image.get_rect(center=(x, y))
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1280 * SCALE, 720 * SCALE))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
-playerPos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-player = MySprite(playerImage, playerPos.x, playerPos.y)
+playerPos = pygame.Vector2(screen.get_width()/ 2, screen.get_height()/ 2)
+player = MySprite(playerImage, playerPos.x, playerPos.y, 25, 25)
 
 while running:
     for event in pygame.event.get():
@@ -32,13 +28,13 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        playerPos.y -= 300 * dt
+        playerPos.y -= 300 * dt * SCALE
     if keys[pygame.K_s]:
-        playerPos.y += 300 * dt
+        playerPos.y += 300 * dt * SCALE
     if keys[pygame.K_a]:
-        playerPos.x -= 300 * dt
+        playerPos.x -= 300 * dt * SCALE
     if keys[pygame.K_d]:
-        playerPos.x += 300 * dt
+        playerPos.x += 300 * dt * SCALE
 
     player.rect.centerx = playerPos.x
     player.rect.centery = playerPos.y
@@ -50,7 +46,6 @@ while running:
     dt = clock.tick(30) / 1000
 
     """
-    pygame.init()
     pygame.display.set_caption(str(playerPos.y))
     pygame.display.flip()
     """
