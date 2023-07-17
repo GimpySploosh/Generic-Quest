413c0c730800726d646377626a183fbb348d9fd9
GimpySploosh <86210959+GimpySploosh@users.noreply.github.com>
2023-07-17 09:36:02 -0400

    Add files via upload

--- introStuff.py
+++ introStuff.py
@@ -3,9 +3,9 @@ import pygame
 playerImage = pygame.image.load("player.png")
 
 # Resize the image to a smaller size
-newWidth = 25
-newHeight = 25
-playerImage = pygame.transform.scale(playerImage, (newWidth, newHeight))
+playerWidth = 25
+playerHeight = 25
+playerImage = pygame.transform.scale(playerImage, (playerWidth, playerHeight))
 
 class MySprite(pygame.sprite.Sprite):
     def __init__(self, image, x, y):
@@ -20,8 +20,8 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-player = MySprite(playerImage, 0, 0)
+playerPos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player = MySprite(playerImage, playerPos.x, playerPos.y)
 
 while running:
     for event in pygame.event.get():
@@ -32,21 +32,27 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        playerPos.y -= 300 * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        playerPos.y += 300 * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        playerPos.x -= 300 * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        playerPos.x += 300 * dt
 
-    player.rect.centerx = player_pos.x
-    player.rect.centery = player_pos.y
+    player.rect.centerx = playerPos.x
+    player.rect.centery = playerPos.y
 
     screen.blit(player.image, player.rect)
 
     pygame.display.flip()
 
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(30) / 1000
+
+    """
+    pygame.init()
+    pygame.display.set_caption(str(playerPos.y))
+    pygame.display.flip()
+    """
 
 pygame.quit()
